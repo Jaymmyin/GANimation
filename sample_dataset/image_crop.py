38b12b7e3c87afb028231cc2f199fbec2327f3e2
Jie Yin <1955971790@qq.com>
2019-02-13 13:27:32 -0800

    Add files via upload

--- sample_dataset/image_crop.py
+++ sample_dataset/image_crop.py
@@ -2,29 +2,48 @@ import cv2
 import numpy as np 
 import os
 import sys
-sys.path.insert(0, 'E:/celebA/GANimation/utils')
+sys.path.insert(0, 'C:\\Files\\Course\\AI 240\\Proj\\GANimation-20190112\\utils')
 import face_utils as face
 
-input_image_path = './sample_dataset/imgs_178/'
-output_image_path = './sample_dataset/imgs_align/'
+#modified by Jie Yin
+# input_image_path  = 'C:\\Files\\Course\\AI 240\\Proj\\wiki_crop\\'
+# output_image_path = 'C:\\Files\\Course\\AI 240\\Proj\\output_wiki_crop\\'
+input_path_list, output_path_list = [], []
+for i in range(24, 100):
+    if i < 10:
+        input_image_path = 'C:\\Files\\Course\\AI 240\\Proj\\wiki_crop\\' + '0' + str(i) + '\\'
+        output_image_path = 'C:\\Files\\Course\\AI 240\\Proj\\output_wiki_crop\\' + '0' + str(i) + '\\'
+        input_path_list.append(input_image_path)
+        output_path_list.append(output_image_path)
+    else:
+        input_image_path = 'C:\\Files\\Course\\AI 240\\Proj\\wiki_crop\\' + str(i) + '\\'
+        output_image_path = 'C:\\Files\\Course\\AI 240\\Proj\\output_wiki_crop\\' + str(i) + '\\'
+        input_path_list.append(input_image_path)
+        output_path_list.append(output_image_path)
 
-image_names = os.listdir(input_image_path)
-error = open('error.txt', 'w')
-for name in image_names:
-    img = cv2.imread(input_image_path+name)
-    name = name.split('.')[0]
-    try:
-        output, M, angle = face.face_crop_and_align(img)
-    except:
+
+for i in range(len(input_path_list)): 
+    print(input_path_list[i])
+    image_names = os.listdir(input_path_list[i])
+    error = open('error.txt', 'w')
+    # print(len(image_names))
+    count = 0
+    for name in image_names:
+        img = cv2.imread(input_path_list[i]+name)
+        name = name.split('.')[0]
         try:
-            bb = face.detect_biggest_face(img)
-            output = img[bb[1]:bb[1]+bb[3], bb[0]:bb[0]+bb[2]]
+            output, face_origin_pos = face.face_crop_and_align(img)
         except:
-            print('cannot find face')
-            continue
-    if output.shape[0] != 128 or output.shape[1] != 128:
-        output = cv2.resize(output, (128,128))
-        # print('resize : ',name)
-    cv2.imwrite(output_image_path+name+'.jpg', output)
-
-error.close()
+            try:
+                bb = face.detect_biggest_face(img)
+                output = img[bb[1]:bb[1]+bb[3], bb[0]:bb[0]+bb[2]]
+            except:
+                count += 1
+                print('cannot find face')
+                continue
+        if output.shape[0] != 128 or output.shape[1] != 128:
+            output = cv2.resize(output, (128,128))
+            # print('resize : ',name)
+        cv2.imwrite(output_path_list[i]+name+'.jpg', output)
+    print(count)
+    error.close()
